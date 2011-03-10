f763f316b1639bbe6f5e1f4e89a0cfe6769957f3
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2011-03-10 12:44:29 +0100

    Fixed dependencies in build

--- project/build/Poju4sProject.scala
+++ project/build/Poju4sProject.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class Poju4s(info:ProjectInfo) extends DefaultProject(info) {
   val junitInterface = "com.novocode" % "junit-interface" % "0.5" % "test->default"
-  val junit = "junit" % "junit" % "4.8.+" % "runtimeb"
+  val junit = "junit" % "junit" % "4.8.1"
 
   override def testOptions = ExcludeTests("poju4s.example.ExampleSpec" :: Nil) :: super.testOptions.toList
 }

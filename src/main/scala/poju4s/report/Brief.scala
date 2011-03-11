4042d0bf2b560afd0ec7fb32a2f4191738e93a63
Joakim Ohlrogge <joakim.ohlrogge@agical.com>
2011-03-11 11:46:42 +0100

    Added possibility to use colors for reports

--- src/main/scala/poju4s/report/Brief.scala
+++ src/main/scala/poju4s/report/Brief.scala
@@ -4,6 +4,10 @@ import poju4s.result._
 import java.util.concurrent._
 
 object Brief {
+  def apply(toRun: List[Callable[Result]]) = new Brief().apply(toRun)
+}
+
+class Brief extends Colorable {
 
   def apply(toRun: List[Callable[Result]]) = {
     def println(str: String) = System.out.println(str);System.out.flush
@@ -11,12 +15,12 @@ object Brief {
 
     val results = toRun.map(x => {
       x.call match {
-        case s: Success => print("."); s
-        case i: Ignored => print("I"); i
-        case x: Fixed => print("X"); x
-        case p: Pending => print("P"); p
-        case f: Failure => print("F"); f
-        case e: Error => print("E"); e
+        case s: Success => print(success(".")); s
+        case i: Ignored => print(ignored("I")); i
+        case x: Fixed => print(fixed("X")); x
+        case p: Pending => print(pending("P")); p
+        case f: Failure => print(failure("F")); f
+        case e: Error => print(error("E")); e
         case r => print("?"); r
       }
     })
--- src/test/scala/poju4s/report/BriefSpec.scala
+++ src/test/scala/poju4s/report/BriefSpec.scala
@@ -4,15 +4,18 @@ import org.junit._
 import org.junit.Assert._
 import poju4s._
 
-class BriefSpec {
+object ReportFixture {
   import poju4s.util.StdXUtil.overrideStdOut
-
   def withExample(body: (Interaction, => String) => Unit) = {
     import poju4s.example.ExampleSpec
     overrideStdOut { output =>
       body(new ExampleSpec, output)
     }
   }
+}
+
+class BriefSpec {
+  import ReportFixture._
 
   @Test
   def marksSuccessWithDot = withExample { (spec, output) =>
@@ -48,12 +51,60 @@ class BriefSpec {
   @Test
   def markIgnoredFixedWithX = withExample { (spec, output) =>
     Brief(spec.run('fixedSpec))
-    assertEquals(summary("X",  1, fixed = 1), output)
+    assertEquals(summary("X", 1, fixed = 1), output)
   }
 
-  def summary(output: String, ran: Int, errors: Int = 0, failures: Int = 0, pending: Int = 0, fixed:Int = 0, ignored: Int = 0) = {
+  def summary(output: String, ran: Int, errors: Int = 0, failures: Int = 0, pending: Int = 0, fixed: Int = 0, ignored: Int = 0) = {
     val nl = System.getProperty("line.separator")
     output + nl +
-      "Ran: " + ran + ", Errors: " + errors + ", Failures: " + failures + ", Pending: " + pending + ", Fixed: "+ fixed + ", Ignored: " + ignored + nl
+      "Ran: " + ran + ", Errors: " + errors + ", Failures: " + failures + ", Pending: " + pending + ", Fixed: " + fixed + ", Ignored: " + ignored + nl
+  }
+}
+
+class BriefSpecWithColor extends Colorable with Color {
+  import ReportFixture._
+  def brief = new Brief with Color
+
+  @Test
+  def marksSuccessWithDot = withExample { (spec, output) =>
+    brief (spec.run('passingSpec))
+
+    assertEquals(summary(success("."), 1), output)
+  }
+
+  @Test
+  def markFailureWithF = withExample { (spec, output) =>
+    brief(spec.run('failingSpec))
+    assertEquals(summary(failure("F"), 1, failures = 1), output)
+  }
+
+  @Test
+  def markErrorWithE = withExample { (spec, output) =>
+    brief(spec.run('errorSpec))
+    assertEquals(summary(error("E"), 1, errors = 1), output)
+  }
+
+  @Test
+  def markPendingWithP = withExample { (spec, output) =>
+    brief(spec.run('pendingSpec))
+    assertEquals(summary(pending("P"), 1, pending = 1), output)
+  }
+
+  @Test
+  def markIgnoredWithI = withExample { (spec, output) =>
+    brief(spec.run('ignoredSpec))
+    assertEquals(summary(ignored("I"), 1, ignored = 1), output)
+  }
+
+  @Test
+  def markIgnoredFixedWithX = withExample { (spec, output) =>
+    brief(spec.run('fixedSpec))
+    assertEquals(summary(fixed("X"), 1, fixed = 1), output)
+  }
+
+  def summary(output: String, ran: Int, errors: Int = 0, failures: Int = 0, pending: Int = 0, fixed: Int = 0, ignored: Int = 0) = {
+    val nl = System.getProperty("line.separator")
+    output + nl +
+      "Ran: " + ran + ", Errors: " + errors + ", Failures: " + failures + ", Pending: " + pending + ", Fixed: " + fixed + ", Ignored: " + ignored + nl
   }
 }

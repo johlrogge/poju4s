4ed4ac5691dca70220eeb87083ab633646301cc8
Enno Runne <enno@runne.net>
2011-03-17 22:38:56 +0100

    introduced filter for whole stack trace

--- src/main/scala/poju4s/report/Verbose.scala
+++ src/main/scala/poju4s/report/Verbose.scala
@@ -1,29 +1,43 @@
-package poju4s.report
+package poju4s
+package report
 
-import poju4s.result._
+import result._
 
-object NoJUnit extends (Option[StackTraceElement] => Option[StackTraceElement]) {
-  def apply(se: Option[StackTraceElement]) = {
-    se.flatMap { x => { if (x.getClassName.contains(".junit.")) None else se } }
+object Filters {
+  type TraceFilter = (String, Array[StackTraceElement]) => Array[StackTraceElement]
+  type TraceElementFilter = Option[StackTraceElement] => Option[StackTraceElement]
+
+  object NoJUnit extends TraceElementFilter {
+    def apply(se: Option[StackTraceElement]) = {
+      se.flatMap { x => { if (x.getClassName.contains(".junit.")) None else se } }
+    }
   }
-}
-object NoSBT extends (Option[StackTraceElement] => Option[StackTraceElement]) {
-  def apply(se: Option[StackTraceElement]) = {
-    se.flatMap { x => { if (x.getClassName.startsWith("sbt.") || x.getClassName.startsWith("xsbt.")) None else se } }
+  object NoSBT extends TraceElementFilter {
+    def apply(se: Option[StackTraceElement]) = {
+      se.flatMap { x => { if (x.getClassName.startsWith("sbt.") || x.getClassName.startsWith("xsbt.")) None else se } }
+    }
   }
-}
-object NoConsole extends (Option[StackTraceElement] => Option[StackTraceElement]) {
-  def apply(se: Option[StackTraceElement]) = {
-    se.flatMap { x =>
-      {
-        if (x.getFileName == "<console>" ||
-          x.getClassName.startsWith("scala.tools.nsc.Interpreter")) None else se
+  object NoConsole extends TraceElementFilter {
+    def apply(se: Option[StackTraceElement]) = {
+      se.flatMap { x =>
+        {
+          if (x.getFileName == "<console>" ||
+            x.getClassName.startsWith("scala.tools.nsc.Interpreter")) None else se
+        }
       }
+    }
+  }
+
+  object StopAtTestClass extends TraceFilter {
+    def apply(group: String, se: Array[StackTraceElement]) = {
+      se.reverse.dropWhile(!_.getClassName.startsWith(group)).reverse
     }
   }
 }
 
-class Verbose(traceFilter: Option[StackTraceElement] => Option[StackTraceElement] = NoJUnit.compose(NoSBT).compose(NoConsole)) extends ReportElement {
+import Filters._
+
+class Verbose(traceFilter: TraceFilter = StopAtTestClass, traceElementFilter: TraceElementFilter = NoJUnit.compose(NoSBT).compose(NoConsole)) extends ReportElement {
   def print(summary: Summary, target: Target) = {
     val s = summary.style
     for (result <- summary) {
@@ -31,23 +45,25 @@ class Verbose(traceFilter: Option[StackTraceElement] => Option[StackTraceElement
         case ErrorDetail(group, spec, ed) => {
           val i = summary.style.icon
           target.println(i(result) + " " + spec.name + " (" + group + ")")
-          def printException(ex: Throwable):Unit = {
+
+          def printException(ex: Throwable): Unit = {
             target.println("  - " + s.emphasis(ex.getMessage))
             target.println("     " + s(result)(ex.getClass.getName))
             for (
-              next <- ex.getStackTrace;
-              se <- traceFilter(Some(next))
+              next <- traceFilter(group, ex.getStackTrace);
+              se <- traceElementFilter(Some(next))
             ) {
               target.println("      at " + s.className(se.getClassName()) + "." +
                 s.method(se.getMethodName) + "(" +
                 s.file(se.getFileName) + ":" +
                 s.line("" + se.getLineNumber) + ")")
             }
-            Option(ex.getCause).foreach{x =>
+            Option(ex.getCause).foreach { x =>
               target.println("    " + s.emphasis("caused by:"))
               printException(ex.getCause)
             }
           }
+
           printException(ed.error)
         }
         case _ => ()
@@ -55,5 +71,7 @@ class Verbose(traceFilter: Option[StackTraceElement] => Option[StackTraceElement
     }
     target.println("")
     summary
+
   }
+
 }
